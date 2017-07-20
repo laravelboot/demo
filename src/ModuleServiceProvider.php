f8cf0430d3631db7cce457267e4e96a010ce7bc4
17huo <mawenpei@17huo.com>
2017-07-20 10:25:37 +0800

    fixed

---
+++ src/ModuleServiceProvider.php
@@ -0,0 +1,68 @@
+<?php
+namespace Module\Demo;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Events\Dispatcher;
+use LaravelBoot\Foundation\Module\Abstracts\Module;
+
+class ModuleServiceProvider extends Module
+{
+	public function __construct(Application $app)
+    {
+        parent::__construct($app);
+    }
+	
+	public function boot()
+    {
+	}
+	
+	/**
+     * Description of module
+     *
+     * @return string
+     */
+    public static function description()
+    {
+        return '示例模块';
+    }
+
+    /**
+     * Install module.
+     *
+     * @return bool
+     */
+    public static function install()
+    {
+        return true;
+    }
+
+    /**
+     * Name of module.
+     *
+     * @return string
+     */
+    public static function name()
+    {
+        return '示例';
+    }
+
+    /**
+     * Uninstall module.
+     *
+     * @return mixed
+     */
+    public static function uninstall()
+    {
+        return true;
+    }
+
+    /**
+     * Version of module.
+     *
+     * @return string
+     */
+    public static function version()
+    {
+        return '1.0.0';
+    }
+}
